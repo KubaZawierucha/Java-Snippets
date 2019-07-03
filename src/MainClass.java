4da367ce29b1506fae8ac118cd8c1ebd949e35e1
KubaZawierucha <j.zawierucha97@gmail.com>
2019-07-03 18:12:03 +0200

    Repository prepared to contain useful Java code fragments. Random number generator function added.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,18 +1,5 @@
-import java.util.Random;
-
+// This class is for testing.
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        MainClass myGenerator = new MainClass();
-        System.out.println(myGenerator.generateInt(1,-5));
-    }
-
-    private int generateInt(int start, int stop) {
-        if (start > stop) {
-            System.out.println("Error!\nStop must be grater or equal to start!");
-            return 0;
-        }
-        Random generator = new Random();
-        return generator.nextInt(stop - start + 1) + start;
     }
 }
---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    int generateInt(int start, int stop) {
+        if (start > stop) {
+            System.out.println("Error!\nStop must be grater or equal to start!");
+            return 0;
+        }
+        Random generator = new Random();
+        return generator.nextInt(stop - start + 1) + start;
+    }
+}
